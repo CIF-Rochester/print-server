7e69557970a6f8fdf944e03ae8440bf4a82e03ce
ecollins27 <ehren.collins@gmail.com>
2024-11-09 14:21:11 -0500

    files are now saved for a week

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
 import os
 import logging
-from logging import exception
 
 from flask import Flask, request, redirect, url_for, render_template
 from paramiko.ssh_exception import SSHException
-from werkzeug.utils import secure_filename
-from python_freeipa import ClientMeta, Client, AuthenticatedSession
 from subprocess import run
 from datetime import datetime
 import subprocess
 import paramiko
-import requests
-from requests.auth import HTTPBasicAuth
+from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = 'FILE_STORE'
 PRINTER_NAME = 'Brother_HL_L3280CDW_series_USB'
+# save time in seconds
+FILE_SAVE_TIME = 7 * 24 * 60 * 60
 
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -47,7 +45,7 @@ def getNumberOfPage(pdfFile):
 # pass the particular printer with `-P` flag to `lpr`
 
 
-def printFile(file,pages,orientation,per_page):
+def printFile(file,pages,orientation,per_page, copies):
     # return 0
     command = ['lpr',file,'-o', 'fit-to-page', '-o', f'number-up={per_page}', '-P', 'Brother_HL_L3280CDW_series_USB']
     #^ Auto fit to page, provide custom scale later
@@ -55,33 +53,50 @@ def printFile(file,pages,orientation,per_page):
         command.extend(['-o',f'page-ranges={pages}'])
     if orientation=='Landscape':
         command.extend(['-o','orientation-requested=4'])
-    ret = run(command)
-    return ret.returncode
+    retCodes = []
+    for i in range(copies):
+        ret = run(command)
+    return retCodes
+
+def update_files():
+    current_time = datetime.now()
+    files = os.listdir(UPLOAD_FOLDER)
+    for file in files:
+        if (current_time - datetime.strptime(file[:file.index("---")],'%H.%M.%S_%d-%m-%Y')).total_seconds() > FILE_SAVE_TIME:
+            os.remove(f"{UPLOAD_FOLDER}/{file}")
+
 
 def upload_file(username):
     retCodes = []
     try:
         pages = request.form.get('pages')
         ornt  = request.form.get('orientation')
-        per_page = request.form.get('perpage')
+        per_page = int(request.form.get('perpage'))
+        copies = int(request.form.get('copies'))
         for file in request.files.getlist('file'):
-
             if not file or file.filename == '':
                 return render_template('result.html', printer=PRINTER_NAME, data="No file attached")
-            filename = secure_filename(file.filename)
+            current_time = datetime.now()
+            time_string_file = current_time.strftime('%H.%M.%S_%d-%m-%Y')
+            time_string_log = current_time.strftime('%I:%M:%S %p %d-%m-%Y')
+            filename = f"{time_string_file}---{secure_filename(file.filename)}"
             newpath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
 
             file.save(newpath)
-            # print(newpath)
 
             # log things
             pg = getNumberOfPage(newpath)
-            txt = f"{username}, [{datetime.now().strftime('%I:%M:%S %p %d-%m-%Y')}] - File: {file}, Total No. of pages: {pg} "
-            if pages: txt += f"Pages to print: {pages} "
-            if per_page: txt += f"Per page: {per_page}"
+            txt = f"[{time_string_log}] {username} - File: {file.filename}"
+            if copies != 1:
+                txt += f", {copies} copies"
+            txt += f", {pg} pages"
+            if pages:
+                txt += f", {pages} pages printed"
+            if per_page != 1:
+                txt += f", {per_page} per page"
             logger.info(txt)
 
-            ret = printFile(newpath,pages,ornt,per_page)
+            ret = printFile(newpath,pages,ornt,per_page, copies)
             retCodes.append(ret)
 
         if all(retCodes)==0:
@@ -92,22 +107,22 @@ def upload_file(username):
     except Exception as e:
         print(e)
         txt = "Unable to submit print job"
-
-    return render_template('result.html', printer=PRINTER_NAME, data=txt)
+    update_files()
+    return render_template('result.html', data=txt)
 
 @app.route('/', methods=['GET', 'POST'])
 def update_file():
     if request.method == 'GET':
-        return render_template('login.html', printer=PRINTER_NAME)
+        return render_template('login.html')
     if request.form['submit-button'] == 'Login':
         username = request.form.get("username")
         password = request.form.get("password")
         try:
             client.connect("citadel.cif.rochester.edu", username=username, password=password, timeout=-1)
         except SSHException:
-            return render_template('login.html', printer=PRINTER_NAME)
+            return render_template('login.html')
         else:
-            return render_template('index.html', printer=PRINTER_NAME, username=username)
+            return render_template('index.html', username=username)
     else:
         return upload_file(request.form.get("username"))
 
