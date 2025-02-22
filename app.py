0dd6f2ce165cfcf28fd8f330b21eba59148b3ca8
ecollins27 <ehren.collins@gmail.com>
2025-02-22 14:06:03 -0500

    Debug=False

--- app.py
+++ app.py
@@ -205,4 +205,4 @@ if __name__ == '__main__':
     # from waitress import serve
     # serve(app,host='0.0.0.0',port=8080)
     # gunicorn -w 4 'web_printer:app' -b '0.0.0.0:8080'
-    app.run(host='0.0.0.0', port=5000,debug=True)
+    app.run(host='0.0.0.0', port=5000,debug=False)
